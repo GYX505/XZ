64207af125c7db6be819cc5326eead9d0064480b
Chenn099 <1600126562@qq.com>
2022-06-25 20:56:02 +0800

    竞赛界面和商城

--- app/src/main/java/com/example/xz/four/FfourthFragment.java
+++ app/src/main/java/com/example/xz/four/FfourthFragment.java
@@ -2,6 +2,7 @@ package com.example.xz.four;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +12,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.xz.R;
 
 public class FfourthFragment extends Fragment {
-
+    private Spinner myspinner;
     private FfourthViewModel mViewModel;
 
     public static FfourthFragment newInstance() {
@@ -25,7 +29,28 @@ public class FfourthFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ffourth, container, false);
+        View view = inflater.inflate(R.layout.commodity,container,false);
+//        Intent intent = new Intent(getActivity(),PicActivity.class);
+//        startActivity(intent);
+        myspinner = (Spinner)view.findViewById(R.id.spinner_1);
+        myspinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String select_item=parent.getItemAtPosition(position).toString();
+                switch (select_item){
+                    case "纵向排序":
+                        Toast.makeText(getActivity(),"已设置成纵向排列",Toast.LENGTH_SHORT).show();
+                        break;
+                    case "瀑布流":
+                        Toast.makeText(getActivity(),"已设置成瀑布流排列",Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        });
+        return view;
     }
 
     @Override
